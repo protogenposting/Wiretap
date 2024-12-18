cf75bc90ccc2d519510f605174211cca31eeb968
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-12-18 12:50:41 -0500

    yes

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9118f24432707159fb427bdf4e4818a380e816a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22cb48f623a817110ff9a1352990bbe04223cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
