d48ba8a3ca7c3967cac26daf9174d79e8ec4e988
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-12-18 11:39:57 -0500

    yipp[ee

---
+++ Client/Api.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Client;
+
+public static class Api{
+    public async static Task<string[]> getUsers()
+    {
+
+    }
+}
---
+++ Client/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void ClickHandler(object sender, RoutedEventArgs args)
+    {
+        message.Text = "Button clicked!";
+    }
+}
